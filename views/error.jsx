e78878fc711d4be74baf1a0ddf0f093135fa48d4
Yulia <yuyarozental@gmail.com>
2022-08-12 15:58:37 -0400

    commit

---
+++ views/error.jsx
@@ -0,0 +1,12 @@
+const React = require('React');
+const Default = require('./layouts/Default')
+
+const Error = () => {
+    return (
+        <Default>
+        <h2>404: Page Doesn't Exist</h2>
+        </Default>
+    );
+};
+
+module.exports = Error;
