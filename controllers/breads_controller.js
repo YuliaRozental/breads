a65fed8660b874147563e2e92baa14409468341d
Yulia <yuyarozental@gmail.com>
2022-08-12 15:25:59 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const breads = express.Router()
+const express = require('express');
+const breads = express.Router();
 const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
       {
-        breads: Bread
+        breads: Bread,
+        title: 'Index page'
       }
     )
   // res.send(Bread)
@@ -14,7 +15,19 @@ breads.get('/', (req, res) => {
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-})  
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
 
-module.exports = breads
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
+module.exports = breads;
--- views/index.jsx
+++ views/index.jsx
@@ -1,15 +1,15 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Index ({breads})  {
+function Index ({breads, title}) {
     return (
-        <Default>
+        <Default title={title}>
   <h2>Index Page</h2>
-  {/* <p>I have {breads[0].name} bread!</p> */}
   {/* This is a JSX comment. */}
+  {/* <p>I have {breads[0].name} bread!</p> */}
   <ul>
     {
-      breads.map((bread, index)=> {
+      breads.map((bread, index) => {
         return (
         <li key={index}>
         <a href={`/breads/${index}`}>
